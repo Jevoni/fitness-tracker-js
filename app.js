6ad327927eabbae7a3e730bb4650d036ce05ca9d
Jevoni <jalenjay23@gmail.com>
2023-01-30 12:18:23 -0500

    Minor changes made

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 })
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
